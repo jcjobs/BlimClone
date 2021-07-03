9c6fbccf30f1285321693a8460f0caa614fe09eb
jcjobs_blim <jperez@blim.com>
2021-07-03 03:32:54 -0500

    Initial Commit

---
+++ BlimClone/BlimCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlimCloneApp.swift
+//  BlimClone
+//
+//  Created by Juan Carlos Perez Delgadillo on 03/07/21.
+//
+
+import SwiftUI
+
+@main
+struct BlimCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
